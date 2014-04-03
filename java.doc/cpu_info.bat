6712f6b252ae61e0c52dbdce5f6252ab09c32938
Guo Xiang <guoshun0321@gmail.com>
2014-04-03 18:06:21 +0800

    jvm

---
+++ java.doc/cpu_info.bat
@@ -0,0 +1,2 @@
+
+typeperf "\Processor(_Total)\% Privileged Time" "\Processor(_Total)\% User Time" "\Processor(_Total)\% Processor Time" "\System\Processor Queue Length" "\Memory\Available MBytes" "\Memory\Pages/sec" -f csv -o output.csv
