a32b47c71188b5fca2b55a026f4604758cef040d
Guo Xiang <guoshun0321@gmail.com>
2014-04-09 18:26:49 +0800

    jvm

--- java.doc/cpu_info.bat
+++ java.doc/cpu_info.bat
@@ -1,2 +1,3 @@
 
 typeperf "\Processor(_Total)\% Privileged Time" "\Processor(_Total)\% User Time" "\Processor(_Total)\% Processor Time" "\System\Processor Queue Length" "\Memory\Available MBytes" "\Memory\Pages/sec" -f csv -o output.csv
+typeperf "\Network Interface(*)\Bytes Total/sec" "\Network Interface(*)\Current Bandwidth/sec" -f csv -o output.csv
