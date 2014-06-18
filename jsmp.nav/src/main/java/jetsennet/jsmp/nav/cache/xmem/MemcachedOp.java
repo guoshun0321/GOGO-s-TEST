20b37e06f5f6649aca5b7f6213d68ca8cc64d3ac
jetsen <jetsen@jetsen.centos175>
2014-06-18 18:22:48 +0800

    a7

--- jsmp.nav/src/main/java/jetsennet/jsmp/nav/cache/xmem/MemcachedOp.java
+++ jsmp.nav/src/main/java/jetsennet/jsmp/nav/cache/xmem/MemcachedOp.java
@@ -18,222 +18,234 @@ import org.slf4j.LoggerFactory;
 public class MemcachedOp
 {
 
-    /**
-     * XMemcached client生成器
-     */
-    private XMemcachedClientBuilder builder;
-    /**
-     * XMemcached client
-     */
-    private volatile MemcachedClient client;
-    /**
-     * 日志
-     */
-    private static final Logger logger = LoggerFactory.getLogger(MemcachedOp.class);
+	/**
+	 * XMemcached client生成器
+	 */
+	private XMemcachedClientBuilder builder;
+	/**
+	 * XMemcached client
+	 */
+	private volatile MemcachedClient client;
+	/**
+	 * 日志
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(MemcachedOp.class);
 
-    private static final MemcachedOp instance = new MemcachedOp();
+	private static final MemcachedOp instance = new MemcachedOp();
 
-    private MemcachedOp()
-    {
-        try
-        {
-            builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(Config.CACHE_SERVERS));
-            //        builder.setFailureMode(true);
-            // 使用二进制文件  
-            builder.setCommandFactory(new BinaryCommandFactory());
-            // 连接池大小
-            builder.setConnectionPoolSize(Config.CACHE_POOLSIZE);
-            client = builder.build();
-        }
-        catch (Exception ex)
-        {
-            logger.error("", ex);
-            client = null;
-        }
-    }
+	private MemcachedOp()
+	{
+		try
+		{
+			builder = new XMemcachedClientBuilder(AddrUtil.getAddresses(Config.CACHE_SERVERS));
+			//        builder.setFailureMode(true);
+			// 使用二进制文件  
+			builder.setCommandFactory(new BinaryCommandFactory());
+			// 连接池大小
+			builder.setConnectionPoolSize(Config.CACHE_POOLSIZE);
+			client = builder.build();
+		}
+		catch (Exception ex)
+		{
+			logger.error("", ex);
+			client = null;
+		}
+	}
 
-    public static MemcachedOp getInstance()
-    {
-        return instance;
-    }
+	public static MemcachedOp getInstance()
+	{
+		return instance;
+	}
 
-    public void deleteAll()
-    {
-        try
-        {
-            client.flushAll();
-        }
-        catch (Exception ex)
-        {
-            this.exceptionHandle(ex);
-        }
-    }
+	public void deleteAll()
+	{
+		try
+		{
+			client.flushAll();
+		}
+		catch (Exception ex)
+		{
+			this.exceptionHandle(ex);
+		}
+	}
 
-    public void put(String key, Object value)
-    {
-        try
-        {
-            client.set(key, Config.CACHE_TIMEOUT, value);
-        }
-        catch (Exception ex)
-        {
-            this.exceptionHandle(ex);
-        }
-    }
+	public void put(String key, Object value)
+	{
+		putTimeout(key, value, Config.CACHE_TIMEOUT);
+	}
+	
+	/**
+	 * 往缓存放置数据，并设置过期时间
+	 * 
+	 * @param key
+	 * @param value
+	 * @param timeout
+	 */
+	public void putTimeout(String key, Object value, int timeout)
+	{
+		try
+		{
+			client.set(key, timeout, value);
+		}
+		catch (Exception ex)
+		{
+			this.exceptionHandle(ex);
+		}
+	}
 
-    public <T> T get(String key)
-    {
-        return this.get(key, false);
-    }
+	public <T> T get(String key)
+	{
+		return this.get(key, false);
+	}
 
-    public int getInt(String key)
-    {
-        return (int) this.get(key, false);
-    }
+	public int getInt(String key)
+	{
+		return (int) this.get(key, false);
+	}
 
-    public <T> T get(String key, boolean isNullable)
-    {
-        Object retval = null;
-        try
-        {
-            retval = client.get(key, Config.CACHE_TIMEOUT);
-        }
-        catch (Exception ex)
-        {
-            this.exceptionHandle(ex);
-        }
-        if (retval == null)
-        {
-            if (isNullable)
-            {
-                return null;
-            }
-            else
-            {
-                throw new UncheckedNavException("取值为null：" + key);
-            }
-        }
-        else
-        {
-            return (T) retval;
-        }
-    }
+	public <T> T get(String key, boolean isNullable)
+	{
+		Object retval = null;
+		try
+		{
+			retval = client.get(key, Config.CACHE_TIMEOUT);
+		}
+		catch (Exception ex)
+		{
+			this.exceptionHandle(ex);
+		}
+		if (retval == null)
+		{
+			if (isNullable)
+			{
+				return null;
+			}
+			else
+			{
+				throw new UncheckedNavException("取值为null：" + key);
+			}
+		}
+		else
+		{
+			return (T) retval;
+		}
+	}
 
-    public List<Integer> getListInt(String key)
-    {
-        List<Integer> retval = null;
-        try
-        {
-            retval = client.get(key, Config.CACHE_TIMEOUT);
-        }
-        catch (Exception ex)
-        {
-            this.exceptionHandle(ex);
-        }
-        return retval;
-    }
+	public List<Integer> getListInt(String key)
+	{
+		List<Integer> retval = null;
+		try
+		{
+			retval = client.get(key, Config.CACHE_TIMEOUT);
+		}
+		catch (Exception ex)
+		{
+			this.exceptionHandle(ex);
+		}
+		return retval;
+	}
 
-    public List<String> getListString(String key)
-    {
-        List<String> retval = null;
-        try
-        {
-            retval = client.get(key, Config.CACHE_TIMEOUT);
-        }
-        catch (Exception ex)
-        {
-            this.exceptionHandle(ex);
-        }
-        return retval;
-    }
+	public List<String> getListString(String key)
+	{
+		List<String> retval = null;
+		try
+		{
+			retval = client.get(key, Config.CACHE_TIMEOUT);
+		}
+		catch (Exception ex)
+		{
+			this.exceptionHandle(ex);
+		}
+		return retval;
+	}
 
-    public Map<String, Object> gets(List<String> keys)
-    {
-        Map<String, Object> retval = null;
-        try
-        {
-            retval = client.get(keys, Config.CACHE_TIMEOUT);
-        }
-        catch (Exception ex)
-        {
-            this.exceptionHandle(ex);
-        }
-        return retval;
-    }
+	public Map<String, Object> gets(List<String> keys)
+	{
+		Map<String, Object> retval = null;
+		try
+		{
+			retval = client.get(keys, Config.CACHE_TIMEOUT);
+		}
+		catch (Exception ex)
+		{
+			this.exceptionHandle(ex);
+		}
+		return retval;
+	}
 
-    public Object del(String key)
-    {
-        Object retval = null;
-        try
-        {
-            retval = client.delete(key);
-        }
-        catch (Exception ex)
-        {
-            this.exceptionHandle(ex);
-        }
-        return retval;
-    }
+	public Object del(String key)
+	{
+		Object retval = null;
+		try
+		{
+			retval = client.delete(key);
+		}
+		catch (Exception ex)
+		{
+			this.exceptionHandle(ex);
+		}
+		return retval;
+	}
 
-    /**
-     * 开启client。同步操作由调用者实现。
-     */
-    public void setup()
-    {
-        try
-        {
-            if (this.client != null)
-            {
-                this.client = builder.build();
-            }
-        }
-        catch (Exception ex)
-        {
-            logger.error("", ex);
-            throw new CacheException(ex);
-        }
-        finally
-        {
-            this.client = null;
-        }
-    }
+	/**
+	 * 开启client。同步操作由调用者实现。
+	 */
+	public void setup()
+	{
+		try
+		{
+			if (this.client != null)
+			{
+				this.client = builder.build();
+			}
+		}
+		catch (Exception ex)
+		{
+			logger.error("", ex);
+			throw new CacheException(ex);
+		}
+		finally
+		{
+			this.client = null;
+		}
+	}
 
-    /**
-     * 关闭client。同步操作由调用者实现。
-     */
-    public void shutdown()
-    {
-        try
-        {
-            this.client.shutdown();
-        }
-        catch (Exception ex)
-        {
-            logger.error("", ex);
-            throw new CacheException(ex);
-        }
-        finally
-        {
-            this.client = null;
-        }
-    }
+	/**
+	 * 关闭client。同步操作由调用者实现。
+	 */
+	public void shutdown()
+	{
+		try
+		{
+			this.client.shutdown();
+		}
+		catch (Exception ex)
+		{
+			logger.error("", ex);
+			throw new CacheException(ex);
+		}
+		finally
+		{
+			this.client = null;
+		}
+	}
 
-    private void exceptionHandle(Exception ex)
-    {
-        if (ex instanceof MemcachedException)
-        {
-            logger.error("Memcached客户端操作失败！", ex);
-            throw new CacheException(ex);
-        }
-        else if (ex instanceof TimeoutException)
-        {
-            logger.error("Memcached客户端操作超时！", ex);
-            throw new CacheException(ex);
-        }
-        else
-        {
-            logger.error("", ex);
-            throw new CacheException(ex);
-        }
-    }
+	private void exceptionHandle(Exception ex)
+	{
+		if (ex instanceof MemcachedException)
+		{
+			logger.error("Memcached客户端操作失败！", ex);
+			throw new CacheException(ex);
+		}
+		else if (ex instanceof TimeoutException)
+		{
+			logger.error("Memcached客户端操作超时！", ex);
+			throw new CacheException(ex);
+		}
+		else
+		{
+			logger.error("", ex);
+			throw new CacheException(ex);
+		}
+	}
 }
